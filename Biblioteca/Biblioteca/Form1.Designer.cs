070f9c4bbe50276f1386c5b2b0bfd054e4972ede
AddamCauê <AddamCau@ADDAM>
2016-10-24 21:24:01 -0200

    Aumento Tela

--- Biblioteca/Biblioteca/Form1.Designer.cs
+++ Biblioteca/Biblioteca/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.glControl1.BackColor = System.Drawing.Color.Black;
             this.glControl1.Location = new System.Drawing.Point(55, 47);
             this.glControl1.Name = "glControl1";
-            this.glControl1.Size = new System.Drawing.Size(150, 150);
+            this.glControl1.Size = new System.Drawing.Size(771, 421);
             this.glControl1.TabIndex = 0;
             this.glControl1.VSync = false;
             this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
@@ -49,7 +49,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(891, 480);
             this.Controls.Add(this.glControl1);
             this.Name = "Form1";
             this.Text = "Form1";
